fd3e2d68d9395b8d6cb8f83ef60ef29c7612b23b
jeferson.oferreira@etec.sp.gov.br <jeferson.oferreira@etec.sp.gov.br>
2022-05-11 20:07:48 -0300

    exemplo de Herança c#

---
+++ HRCA1105/Funcionario.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRCA1105
+{
+    class Funcionario : Pessoa
+    {
+        public int registro;
+        protected double salario;
+        public double inss;
+        protected double salarioLiquido;
+
+        
+
+
+        public void registraSalario(double valor)
+        {
+            this.salario = valor;
+        }
+
+        public void descontoInss()
+        {
+            this.inss = this.salario * 0.11;
+           
+        }
+        public void mostraDescontoInss()
+        {
+            Console.WriteLine("Desconto:" + this.inss);
+        }
+        public void mostraEmail()
+        {
+            Console.WriteLine("E-mail:{0}",this.email);
+        }
+
+        public void mostraSalarioBruto()
+        {
+            Console.WriteLine("Salario:{0}", this.salario);
+        }
+
+        public void mostraSalarioLiquido()
+        {
+            this.salarioLiquido = this.salario - this.inss;
+            Console.WriteLine("Salario Liquido:{0}", this.salarioLiquido);
+        }
+
+       
+        public void mostraRegistro()
+        {
+            Console.WriteLine("Registro:{0}", this.registro);
+        }
+
+        public void exibirFuncionario()
+        {
+            this.mostraNome();
+            this.mostraSalarioBruto();
+            this.mostraDescontoInss();
+            this.mostraSalarioLiquido();
+        }
+    }
+}
---
+++ HRCA1105/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRCA1105
+{
+    class Pessoa
+    {
+        public string nome;
+        public string email;
+        public string telefone;
+        public string cpf;
+        public string dataNasc;
+        // falar sobre retorno.
+        public void mostraNome()
+        {
+            Console.WriteLine("nome:{0}",this.nome);
+        }
+
+    }
+}
